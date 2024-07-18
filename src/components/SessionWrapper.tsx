7600f9625b3110bc5cb957312cc968c77ad2eb80
ahmedmohamed07 <ahmed.mohamed197707@gmail.com>
2024-07-18 09:23:13 +0300

    Install and implement recoil

--- src/components/SessionWrapper.tsx
+++ src/components/SessionWrapper.tsx
@@ -2,12 +2,17 @@
 
 import { SessionProvider } from 'next-auth/react';
 import { ReactNode } from 'react';
+import { RecoilRoot } from 'recoil';
 
 interface SessionWrapperProps {
   children: ReactNode;
 }
 
 const SessionWrapper = ({ children }: SessionWrapperProps) => {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <RecoilRoot>{children}</RecoilRoot>
+    </SessionProvider>
+  );
 };
 export default SessionWrapper;
